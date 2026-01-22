47d67de1b9e1e85cff740bd67ee9d1e37952922e
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2026-01-22 13:02:57 +0900

    refactor: remove middleware for redirection

--- src/routers/base.ts
+++ src/routers/base.ts
@@ -1,6 +1,4 @@
 import { Router } from "router";
 import { TrailingSlash } from "router/trailing-slash";
-import Redirect from "@/handlers/redirect/middleware.ts";
 
-export default new Router().use(new TrailingSlash("never"))
-  .use(new Redirect());
+export default new Router().use(new TrailingSlash("never"));
