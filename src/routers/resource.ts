58de58f7f10fc4715cb6b5f3a22fdc9f36d34f74
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2026-01-26 15:00:48 +0900

    refactor: remove endpoint for robots

--- src/routers/resource.ts
+++ src/routers/resource.ts
@@ -1,7 +1,5 @@
 import { Router } from "router";
 import sitemap from "@/handlers/sitemap/handler.ts";
-import robots from "@/handlers/robots/handler.ts";
 
 export default new Router()
-  .get("/robots.txt", robots)
   .get("/sitemap.xml", sitemap);
