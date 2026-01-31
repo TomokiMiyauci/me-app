fff2747e5f6285fb78a4a4be5407106c88dbaa39
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2026-01-31 17:24:39 +0900

    refactor: rename file

--- src/framework/entry.rsc.tsx
+++ src/framework/entry.rsc.tsx
@@ -1,16 +1,8 @@
 import type * as ssr from "./entry.ssr.tsx";
 import resourceRouter from "@/routers/resource.ts";
-import { fromFileUrl } from "@std/path";
-import StaticDir from "router/static-dir";
 import App from "@/handlers/app//middleware.ts";
 
 let router = resourceRouter;
-
-if (import.meta.env.PROD) {
-  const clientDir = fromFileUrl(import.meta.vite.outDir.resolve("client"));
-
-  router = router.use(new StaticDir(clientDir));
-}
 
 const { renderHtmlStream } = await import.meta.viteRsc.loadModule<
   typeof ssr
@@ -24,7 +16,7 @@ router = router.use(
 
 export default {
   fetch: router.fetch.bind(router),
-} satisfies Deno.ServeDefaultExport;
+};
 
 if (import.meta.hot) {
   import.meta.hot.accept();
