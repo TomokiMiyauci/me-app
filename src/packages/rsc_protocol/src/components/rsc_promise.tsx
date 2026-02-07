adba9eda991593305e673820024ccc2a3b8317f2
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2026-02-07 14:00:42 +0900

    perf(rsc-protocol): switch to async-await component

--- src/packages/rsc_protocol/src/components/rsc_promise.tsx
+++ src/packages/rsc_protocol/src/components/rsc_promise.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode, use } from "react";
+import type { ReactNode } from "react";
 import type { RscPayload } from "../types.ts";
 import Rsc from "./rsc.tsx";
 
@@ -6,8 +6,10 @@ export interface RscPromiseProps {
   promise: Promise<RscPayload>;
 }
 
-export default function RscPromise(props: RscPromiseProps): ReactNode {
-  const payload = use(props.promise);
+export default async function RscPromise(
+  props: RscPromiseProps,
+): Promise<ReactNode> {
+  const payload = await props.promise;
 
   return <Rsc payload={payload} />;
 }
