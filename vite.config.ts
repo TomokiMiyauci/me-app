3f7f009b12d4560df95f4b241899cc8df90a82bf
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2026-02-15 00:01:18 +0900

    refactor: remove unused polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import rsc from "@vitejs/plugin-rsc";
 import react from "@vitejs/plugin-react";
-import { defineConfig, perEnvironmentPlugin } from "vite";
+import { defineConfig } from "vite";
 import { nodeEnv } from "vite-node-env";
 import rscAssets from "vite-plugin-rsc-assets-manifest";
 import { manifest, outDirResolve } from "vite-plugin-manifest";
-import inject from "@rollup/plugin-inject";
 import { nodeScheme } from "vite-node-scheme";
 import deno from "vite-plugin-deno";
 import codegen from "vite-plugin-graphql-codegen";
@@ -43,16 +42,6 @@ export default defineConfig({
     rscAssets,
     manifest,
     outDirResolve,
-    perEnvironmentPlugin("inject", (env) => {
-      if (env.name === "client") {
-        // This is deno bug
-        // deno-lint-ignore no-explicit-any
-        return (inject as any)({
-          "Deno.env": "@/framework/polyfills/deno_env.ts",
-        });
-      }
-      return false;
-    }),
     codegen({
       configFilePathOverride: "./.graphqlrc",
     }),
