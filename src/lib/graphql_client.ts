3f9207bca24d96963d6035aa151256ab4363c729
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2026-02-16 14:26:42 +0900

    fix: fix to recursive import

--- src/lib/graphql_client.ts
+++ src/lib/graphql_client.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from "graphql-client";
 import { Cache } from "graphql-client/cache";
 import { CONTENT_ENDPOINT } from "~env";
-import { cache } from "~lib";
+import cache from "./cache.ts";
 
 const client = new GraphQLClient(CONTENT_ENDPOINT, {
   middleware: [new Cache(cache)],
