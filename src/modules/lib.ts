ed3cc0bc5ea881f0f20f178ece006434583ddf77
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2026-02-22 02:22:30 +0900

    chore: remove unused

--- src/modules/lib.ts
+++ src/modules/lib.ts
@@ -1,3 +1,2 @@
-// export { default as gqlClient } from "../lib/graphql_client.ts";
 export { default as cache } from "../lib/cache.ts";
 export { default as apolloClient } from "../lib/apollo_client.ts";
